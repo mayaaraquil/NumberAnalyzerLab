70a1b43990966677bc654822a9df2be6fec1ba12
Maya Araquil <mayaaraquil775@gmail.com>
2023-07-12 02:49:36 -0400

    NumberAnalyzerLab

---
+++ NumberAnalyzer/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Hello there! welcome to decision maker ");
+{
+    int num = 0;
+    Console.WriteLine("Enter a number between from 1-100");
+    num = int.Parse(Console.ReadLine());
+    if (num % 2 == 0)
+    {
+        Console.WriteLine("{0} is even", num);
+    }
+    else
+    {
+        Console.WriteLine("{0} is odd", num);
+    }
+    Console.ReadLine();
+}
+
+
+
+
